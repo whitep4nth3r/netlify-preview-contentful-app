ecb29b4a1f0cb01d10831253ba3e80b7b3291034
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-18 13:53:08 +0000

    TRY SOMETHING ELSE

--- functions/deployme.js
+++ functions/deployme.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 
-export const handler = async (event) => {
+exports.handler = async (event) => {
   const response = await fetch(process.env.REACT_APP_NETLIFY_BUILD_HOOK, {
     method: "POST",
   });
